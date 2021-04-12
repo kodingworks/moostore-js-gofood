de29d78e669c0ec3d9ac29a2cf53157df8ee0e3e
Mohamad Rafi <rafi.randoni@gmail.com>
2021-04-12 22:35:45 +0700

    fix(example): adjust code

--- example/usage.ts
+++ example/usage.ts
@@ -1,18 +1,35 @@
 import MoostoreJS from 'moostorejs'
 import Configuration from 'moostorejs/configuration'
 import Gofood from '..'
+import * as fs from 'fs'
+const cheerio = require('cheerio')
 
 export default class Usage {
   main() {
-    const moostore = new MoostoreJS
+    const moostore = new MoostoreJS()
 
     const gofood: Gofood = new Gofood(moostore)
 
     console.log('Fetch start')
 
-    gofood
+    /* gofood
       .shop()
       .getFromURL(
+        'https://gofood.co.id/bandung/restaurant/soto-sate-ayam-pa-somad-karees-timur-6f98f851-e797-471a-bb72-31c6c85ef09d'
+      )
+      .then((response) => {
+        const data = response.getData()
+        console.log('Data fetched')
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      }) */
+
+
+    gofood
+      .shop()
+      .getProducts(
         'https://gofood.co.id/bandung/restaurant/soto-sate-ayam-pa-somad-karees-timur-6f98f851-e797-471a-bb72-31c6c85ef09d'
       )
       .then((response) => {
